ebb8982339aa31fc2bc17f8904731d76d3c7ff7b
Es1amMohamed <EslamMohamedMetwally1@gmail.com>
2022-05-06 18:31:55 +0200

    'code_chat_app'

--- chat.py
+++ chat.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client 
  
 account_sid = 'AC2223ff846eed8487296e1735d82d3583' 
-auth_token = 'bc7b87d613ce3e85bc446793d4c1dcde' 
+auth_token = '' 
 client = Client(account_sid, auth_token) 
 def send():
     message = client.messages.create( 
